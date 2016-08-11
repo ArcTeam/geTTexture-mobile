337c43ee810a8c21462df8a23794d10a1edcd703
Luca Bezzi <luca.bezzi@arc-team.com>
2016-08-11 10:20:28 +0200

    Create Screen1.scm

---
+++ src/appinventor/ai_luca_bezzi/geTTexture/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"158","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"19","AlignHorizontal":"3","AppName":"geTTexture","Scrollable":"True","Sizing":"Responsive","Title":"geTTexture","Uuid":"0","$Components":[{"$Name":"WebViewer1","$Type":"WebViewer","$Version":"6","HomeUrl":"http:\/\/91.121.82.80\/geTexture\/","Uuid":"-727396325"}]}}
+|#
